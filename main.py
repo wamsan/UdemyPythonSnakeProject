b46a56bc8feeb27898e066c644ad1c2b73cb9a5e
wamsan <123329206+wamsan@users.noreply.github.com>
2023-03-20 22:35:59 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -30,16 +30,17 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.score_counter()
+        scoreboard.increase_score()
 
     # Hit Wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
+    # Hit Body
     for segment in snake.segments[:0:-1]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -6,17 +6,29 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.penup()
         self.goto(0, 270)
         self.color("white")
-        self.write(f"Score: {self.score}", align="center", font=('Courier', 18, 'normal'))
+        self.score_counter()
 
     def score_counter(self):
         self.clear()
-        self.score += 1
-        self.write(f"Score: {self.score}", align="center", font=('Courier', 18, 'normal'))
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=('Courier', 18, 'normal'))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("YOU ARE DEAD", align="center", font=('Courier', 18, 'normal'))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as new_data:
+                new_data.write(f"{self.high_score}")
+            self.score = 0
+            self.score_counter()
+
+    def increase_score(self):
+        self.score += 1
+        self.score_counter()
+
+
+
